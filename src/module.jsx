cc0ff379e7476c31479a775a3dc32573764e0112
Anton Semenikhin <protest_the_hero@mail.ru>
2022-07-14 13:28:18 +0300

    create react bundle

---
+++ src/module.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import code from './img/code.jpg';
+const Module = () => (
+  <>
+    <h1>Hello, React!</h1>
+    <img src={code} width={600} />
+  </>
+);
+
+export default Module;
